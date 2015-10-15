3b62a6944e774b480f5a960a575e66025081b364
Ariel Churi <git@sparklelabs.com>
2015-10-15 15:53:49 -0400

    changed fade function to use new system

--- Nightlight2/Nightlight2.ino
+++ Nightlight2/Nightlight2.ino
@@ -1,16 +1,37 @@
+// Set up capacitive sensor
 #include <CapacitiveSensor.h> // the capacitive sensor library
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4, 2); //setup pins 4 and 2 for the sensor. 2 has the probe.
+
+// Set up LED display
+int ledState;
+int ledSpeed = 6000; // Speed at which the LEDs animate.
+byte maxBrightness = 255; // Maximum brightness for LEDs.
+byte ledsVal[] = {0, 0, 0, 0, 0, 0}; // The brightness value of all of the pins.
+byte ledsDir[] = {0, 0, 0, 0, 0, 0};
+/*
+ * You can set a behavior for an LED.
+ * 0 = fade out or stay off.
+ * 1 = fade off and on starting upwards.
+ * 2 = fade off and on starting downwards.
+ * 3 = fade up or stay on.
+ * Example:
+ * ledsVal[2] = 127;
+ * ledsDir[2] = 3;
+ * These two lines would set the LED on pin 6 to medium brightness and tell it to fade up to max brightness.
+ */
+
+ // Variables for writing to the LEDs. You should not need to change these:
 int leds[] = { 3, 5, 6, 9, 10, 11}; // array of all the pwm pins
-byte ledsVal[] = {10, 60, 120, 160, 200, 255};
-byte ledsDir[] = {0, 1, 2, 1, 1, 2};
+int fadeSpeed = 0; // length of time for when to change the fade out of the led
 int fadePreviousMillis;
 int pinCount = 6; // how many pwm pins
 int ledPin = 13; // use the Arduino's built-in led for debugging
+int prevLedTime; // a variable for the fade timing
+
+// Variables for logic
 int i; // a variable later use in the logic
 int prevTime; // a variable for the logic timing
 int interval = 500; // length of time for logic time
-int fadeSpeed = 5; // length of time for when to change the fade out of the led
-int prevFadeTime; // a variable for the fade timing
 int touched;
 int stateVar;
 int lightVar;
@@ -23,17 +44,15 @@ void setup() {
   pinMode(ledPin, OUTPUT); // set the debug led to output
   Serial.begin(9600);
   Serial.println();
-  debug();
+ // debug();
 }
 
 void loop() {
   darkVar = darkSense();
   touchSense(); // call the function "touchSense" and put the result in touched
-
   logic(); // call the function "logic"
-
-//  debug();
-  //fade(); // call the function "fade"
+// debug();
+  fade(); // call the function "fade"
   pulseLED();
   // display(); // call the function "display"
 
---
+++ Nightlight2/display.ino
@@ -0,0 +1,60 @@
+int fade() { // a function to reduce the brightness of leds so they fade out.
+    int currentTime = millis(); // check the time
+    if (currentTime - prevLedTime >= ledSpeed) { //if enough time has gone by:
+        ledsDir[ledState] = 0;
+        ledState++;
+        if (ledState == 6) ledState = 0;
+        ledsDir[ledState] = 3;
+        prevLedTime = currentTime;
+    }
+}
+
+int debug() { // receive a variable when called and put it into "x"
+    Serial.print("stateVar =\t");
+    Serial.print(stateVar);
+    Serial.print("\ttouched =\t");
+    Serial.print(touched);
+    Serial.print("\tlightVar =\t");
+    Serial.print(lightVar);
+    Serial.print("\tdarkVar =\t");
+    Serial.print(darkVar);
+    Serial.println();
+}
+
+int display() { // send the ledFade amount to each of the leds
+    for (int n = 0; n < 6; n++) {
+        analogWrite(leds[n], ledsVal[n]);
+    }
+}
+
+int pulseLED() {
+    unsigned long currentMillis = millis();
+    if (currentMillis - fadePreviousMillis >= fadeSpeed) {
+        for (int i = 0; i < pinCount; i++) {
+            switch (ledsDir[i]) {
+                case 0: // stay at 0 or fade down to 0
+                    if (ledsVal[i] > 0) ledsVal[i]--;
+                    break;
+                case 1: // fading up and down - up
+                    ledsVal[i]++;
+                    if (ledsVal[i] >= 255) ledsDir[i] = 2;
+                    break;
+                case 2: // fading up and down - down
+                    ledsVal[i]--;
+                    if (ledsVal[i] <= 0) ledsDir[i] = 1;
+                    break;
+                case 3: // stay at 255 or fade up to 255
+                    if (ledsVal[i] < 255) ledsVal[i]++;
+                    break;
+                default:
+                    Serial.println("default");
+                    break;
+            }
+            int x = map(ledsVal[i], 0, 255, 0, maxBrightness);
+            analogWrite(leds[i], x);
+            Serial.println(ledsVal[1]);
+            fadePreviousMillis = currentMillis;
+        }
+    }
+}
+
