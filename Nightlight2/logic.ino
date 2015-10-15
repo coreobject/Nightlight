def3bd1bf39da7d079ab24e4b9c258290fc0edb8
Ariel Churi <git@sparklelabs.com>
2015-10-15 16:29:29 -0400

    touch sensor works with maxBrightness

--- Nightlight2/logic.ino
+++ Nightlight2/logic.ino
@@ -2,6 +2,7 @@ int logic() {
   switch (stateVar) {
     case (0): // daytime and lights are off
       lightVar = 0;
+      if (maxBrightness > 0) maxBrightness--;
       if (touched == 1) {
         stateVar = 1;
         touched = 2;
@@ -9,6 +10,7 @@ int logic() {
       break;
     case (1): // touched
       lightVar = 1;
+      if (maxBrightness < 255) maxBrightness++;
       if (touched == 1) {
         stateVar = 0;
         touched = 2;
